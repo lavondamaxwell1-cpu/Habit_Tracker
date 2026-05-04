7b3c4b9ae997aa05054279ae85f6f9e80b1e45d4
Lavonda Maxwell <lavondamaxwell1@gmail.com>
2026-05-04 09:39:22 -0400

    fixed a bug on my login

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL + "/auth";
+const API_URL = `${import.meta.env.VITE_API_URL}/auth`;
 
 export const registerUser = async (data) => {
   const response = await axios.post(`${API_URL}/register`, data);
