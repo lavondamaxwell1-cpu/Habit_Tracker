62893da3463881cd49f009ddbe68d42e84519c95
Lavonda Maxwell <lavondamaxwell1@gmail.com>
2026-05-03 23:50:33 -0400

    use env api url

--- client/src/api/habits.js
+++ client/src/api/habits.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/habits";
+const API_URL = import.meta.env.VITE_API_URL + "/habits";
 
 const getConfig = () => {
   const token = localStorage.getItem("token");
