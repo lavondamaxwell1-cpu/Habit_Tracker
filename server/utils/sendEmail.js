c56404cf81909fb1204eeefed69f7970914f118b
Lavonda Maxwell <lavondamaxwell1@gmail.com>
2026-05-05 10:17:33 -0400

    fix email transporter

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -10,7 +10,7 @@ const sendEmail = async ({ to, subject, text }) => {
   });
 
   await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+    from: `"Habit Tracker" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     text,
